092e3e5e272ae9742b59616aa986be05f7398c58
Waldirio M Pinheiro <wpinheir@ironman.home>
2018-09-26 07:12:06 -0700

    code updated / checked following pylint advice

--- rvtools/corerv.py
+++ rvtools/corerv.py
@@ -1,32 +1,29 @@
+""" Module to read the conf file / vcenter, username and password """
 import re
 
-class coreCode(object):
+class CoreCode(object):
+    """  Main Class *CoreCode* responsible for read the conf file feature """
 
     def read_conf_file(self):
+        """ Definition to read the conf file rvtools.conf """
         try:
-            fp_conf_file = open("rvtools/rvtools.conf","r")
+            fp_conf_file = open("rvtools/rvtools.conf", "r")
             for line in fp_conf_file:
-                if re.search('^vcenter',line):
+                if re.search('^vcenter', line):
                     # VCENTER = re.split('=', re.search('^vcenter',line).string)[1]
-                    self._vcenter = re.split('=', re.search('^vcenter',line).string)[1].strip()
-                if re.search('^username',line):
+                    self._vcenter = re.split('=', re.search('^vcenter', line).string)[1].strip()
+                if re.search('^username', line):
                     # USERNAME = re.split('=', re.search('^username',line).string)[1]
-                    self._username = re.split('=', re.search('^username',line).string)[1].strip()
-                if re.search('^password',line):
+                    self._username = re.split('=', re.search('^username', line).string)[1].strip()
+                if re.search('^password', line):
                     # PASSWORD = re.split('=', re.search('^password',line).string)[1]
-                    self._password = re.split('=', re.search('^password',line).string)[1].strip()
-
-
-            # print(self._vcenter)
-            #print(f.read())
+                    self._password = re.split('=', re.search('^password', line).string)[1].strip()
             return self
         except FileNotFoundError:
             print("There isn't the conf file, please create a new one")
             print("according example below:")
-            print ("-----------------------")
-            print ("vcenter=<fqdn>")
-            print ("username=<vcenter username>")
-            print ("password=<password>")
-            print ("-----------------------")
-
-
+            print("-----------------------")
+            print("vcenter=<fqdn>")
+            print("username=<vcenter username>")
+            print("password=<password>")
+            print("-----------------------")
--- rvtools/rvtools.py
+++ rvtools/rvtools.py
@@ -1,82 +1,39 @@
+""" Main rvtools module """
 #!/usr/bin/env python 
+
+import ssl
+import requests
+from pyVim import connect
 
 from corerv import *
 from vinfo.vinfo import *
-import requests
 
-from pyVim import connect
-from pyVmomi import vmodl
-#from pyVmomi import vim
-import ssl
+#from pyVmomi import vmodl
+
 
 requests.packages.urllib3.disable_warnings()
 
 def main():
+    """ Def responsible to start the vCenter connection and call all report modules """
     print("Reading the conf file")
 
-    obj = coreCode()
+    obj = CoreCode()
     conn = obj.read_conf_file()
 
-    sslContext = ssl._create_unverified_context()
+    ssl_context = ssl._create_unverified_context()
 
-    # sslContext = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-    # sslContext.verify_mode = ssl.CERT_NONE
+    print("vcenter: {}\nuser: {}\npassword: {}\n ".format( \
+         conn._vcenter, conn._username, conn._password))
 
-    print("vcenter: {}\nuser: {}\npassword: {}\n ".format(conn._vcenter, conn._username, conn._password))
-    
-    si = connect.SmartConnect(host=conn._vcenter,user=conn._username,pwd=conn._password, port=443, sslContext=sslContext)
-
-#    content = si.RetrieveContent()
-#    container = content.rootFolder
-
-#    viewType = [vim.VirtualMachine]
-#    containerView = content.viewManager.CreateContainerView(container,viewType,True)
-
-    #vm_data = pchelper.collect_properties()
+    service_instance = connect.SmartConnect(host=conn._vcenter, user=conn._username, \
+         pwd=conn._password, port=443, sslContext=ssl_context)
 
 
-    vinfo_collect(si)
-#    children = containerView.view
-#    for child in children:
-#        machine_name = child.name
-#        print("Machine Name: {}".format(machine_name))
-        #aux = child.config.bootOptions.networkBootProtocol
-#        aux = child.config.hardware.numCPU
-#        print("Aux: {}".format(aux))
-#        pass
-        #print("{}".format(child.config.cpuAllocation))
-        #print("{}".format(child.hardware.numCPU))
-        
-
-#        print("{},{},{}".format(child.name,child.runtime.powerState,child.config.cpuAllocation.hardware.numCPU))
-        #print(child.runtime.powerState)
-##        print(child.guest)
-##        print("")
-
-#    viewType = [vim.Datacenter]
-#    containerView = content.viewManager.CreateContainerView(container,viewType,True)
-
-#    aux = containerView.view
-
-#    print ("test")
-#    pass
+    # VM Information
+    vinfo_collect(service_instance)
 
 
-    # si1 = vim.GuestOsDescriptor.GuestOsIdentifier
-    # si1 = vim.SmartConnect(host=conn._vcenter,user=conn._username,pwd=conn._password, port=443, sslContext=sslContext)
-    
-    # dir(si)
-    # dc = si.content.rootFolder.childEntity[0]
-    # vm = dc.vmFolder.childEntity
 
-    # for i in vm:
-    #     print(i.name)
-    # print(si.CurrentTime())
-
-    #xpto = si.vim.vm.GuestOsDescriptor.GuestOsIdentifier
-
-    pass
-    
 # https://code.vmware.com/apis/358/vsphere
 
 if __name__ == "__main__":
