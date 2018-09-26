dcfaae28b2e1f95747f3b93daf9d0be13f9d0fa3
Waldirio M Pinheiro <wpinheir@ironman.home>
2018-09-26 07:11:07 -0700

    csv module updated

---
+++ rvtools/print/csv_print.py
@@ -0,0 +1,25 @@
+""" Module responsible to save the content as csv file """
+import csv
+
+def csv_print(module_name, server_list):
+    """
+    Def which will receive the server list and then will
+    populate all fields from header as the content.
+
+    Note. All fields will be populated automatically, then according to
+    the module which will call csv_print, the # of fields will be updated
+    automatically.
+    """
+
+    print("Inside Print definition")
+
+    with open(module_name, 'w', newline='') as f:
+        fieldnames = server_list[0].keys()
+        thewriter = csv.DictWriter(f, fieldnames=fieldnames)
+        thewriter.writeheader()
+
+        for each_line in server_list:
+            csv_data_fields = {}
+            for each_field in fieldnames:
+                csv_data_fields[each_field] = each_line[each_field]
+            thewriter.writerow(csv_data_fields)
